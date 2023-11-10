a3e3629c0d2acad0fe1c5b5a4ef94a7b93263cf3
Giselle <milanoregina8@gmail.com>
2023-11-10 17:43:12 -0300

    New media query to change columns quantity in md

--- src/components/SkillsGrid.js
+++ src/components/SkillsGrid.js
@@ -8,7 +8,7 @@ export default function SkillsGrid() {
         <h1 className="w-full text-center text-neutral-50 text-2xl">Skills</h1>
         <hr className="w-32 h-0.5 mx-auto mt-2 bg-[#c3467b] border-0 rounded-full"></hr>
       </div>
-      <ul className="grid xl:grid-cols-6 lg:grid-cols-6 grid-cols-2 pl-8 pr-8 xl:pr-0 lg:pr-0 xl:pl-0 lg:pl-0 items-center justify-center gap-3">
+      <ul className="grid xl:grid-cols-6 lg:grid-cols-6 md:grid-cols-3 grid-cols-2 pl-8 pr-8 xl:pr-0 lg:pr-0 xl:pl-0 lg:pl-0 items-center justify-center gap-3">
         <li className="skill-element-violet">Javascript</li>
         <li className="skill-element-pink">Typescript</li>
         <li className="skill-element-red">Node.js</li>

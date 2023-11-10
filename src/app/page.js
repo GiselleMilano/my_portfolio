a5eb51b2122d15e5aa954e1592cb0b41bb3ab870
Giselle <milanoregina8@gmail.com>
2023-11-10 17:18:21 -0300

    Greeting component code added

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
 import Navbar from "@/components/Navbar";
+import Greeting from "@/components/Greeting";
 
 export default function Home() {
   return (
     <main className="w-full h-full overflow-auto bg-neutral-800">
       <Navbar />
-      <div className="flex flex-col gap-12 xl:mt-14 lg:mt-14"></div>
+      <div className="flex flex-col gap-12 xl:mt-14 lg:mt-14">
+        <Greeting />
+      </div>
     </main>
   );
 }
---
+++ src/components/Greeting.js
@@ -0,0 +1,34 @@
+import Image from "next/image";
+
+export default function Greeting() {
+  return (
+    <section
+      id="home"
+      className="flex flex-col items-center justify-center w-full"
+    >
+      <div className="flex flex-col xl:flex-row-reverse lg:flex-row-reverse items-center justify-center xl:gap-12 lg:gap-12 gap-8 bg-gradient-to-r from-[#9a42d8ff] to-[#d94848ff] w-full pl-12 pr-12 pt-16 xl:pb-0 lg:pb-0 pb-16">
+        <div className="flex justify-center items-center">
+          <Image
+            src="/photo.png"
+            width={200}
+            height={200}
+            className="xl:w-48 xl:h-48 lg:h-48 lg:w-48 w-32 h-32 rounded-full bg-neutral-800 -rotate-12"
+            alt="My Photo"
+          />
+        </div>
+        <div className="h-full text-2xl xl:text-left lg:text-left text-center text-neutral-50">
+          <p>
+            Hi! My name's Giselle.{" "}
+            <img
+              src="/my_emoji_white.svg"
+              alt="emoji"
+              className="w-6 h-6 inline ml-1"
+            />
+          </p>
+          <p>Welcome to my web portfolio!</p>
+        </div>
+      </div>
+      <img src="/wave.svg" alt="Wave svg" className="w-full" />
+    </section>
+  );
+}
